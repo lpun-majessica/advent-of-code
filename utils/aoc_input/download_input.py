d84819136b403e2490c2c29f5b7d44b74e0e8f27
Majestique <nhi.lpu1205@gmail.com>
2024-10-23 15:34:39 +0700

    adjust logic

--- utils/aoc_input/download_input.py
+++ utils/aoc_input/download_input.py
@@ -8,28 +8,35 @@ Get your session cookie to retrieve the correct input!
 Go to any AoC input link > `Inspect` > `Network` tab > Refresh the page > Find `Cookie` header > Copy the text after `session=` :)
 """.strip()
 
-def download_input(year: Union[int, str], day: Union[int, str], session_cookie: str = None):
+def download_input(
+    year: Union[int, str], day: Union[int, str], session_cookie: str = None
+):
     if isinstance(year, int):
         year = str(year)
-    
+
     if isinstance(day, int):
         day = str(day)
-    day = day.lstrip('0')
-    
+    day = day.lstrip("0")
+
     if session_cookie is None:
         session_cookie = get_session_cookie()
     assert session_cookie, assert_message
-    
+
     s = requests.Session()
-    r = s.get(f'https://adventofcode.com/{year}/day/{day}/input', cookies = {'session': session_cookie})
-    inp = r.text
-    
-    return inp.split('\n')[:-1]
+    r = s.get(
+        f"https://adventofcode.com/{year}/day/{day}/input",
+        cookies={"session": session_cookie},
+    )
+    inp = r.text.split("\n")
+
+    return inp[:-1] if not inp[-1] else inp
 
 
-def get_session_cookie(path: Union[Path, str] = '../utils/aoc_input/session_cookie.txt'):
+def get_session_cookie(
+    path: Union[Path, str] = "../utils/aoc_input/session_cookie.txt"
+):
     try:
-        with Path(path).open(mode ='r') as file:
+        with Path(path).open(mode="r") as file:
             return file.read()
     except FileNotFoundError:
         return None
